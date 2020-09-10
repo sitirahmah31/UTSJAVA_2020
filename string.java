53b7842370c23cafdff4c943111c5aca8f273d31
Siti Rahmah <71058659+sitirahmah31@users.noreply.github.com>
2020-09-10 13:28:59 +0700

    Add files via upload

---
+++ string.java
@@ -0,0 +1,49 @@
+package UTS;
+
+public class string {
+	/*
+	 * Siti Rahmah
+	 * XII RPL B
+	 */
+
+	public static void main (String [] args) {
+		String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+
+		char s = str.charAt(18);
+		System.out.println(s);
+		
+		char i = str.charAt(8);
+		System.out.println(i);
+		
+		char t = str.charAt(19);
+		System.out.println(t);
+		
+		char ii = str.charAt(8);
+		System.out.println(ii);
+		
+		char v = str.charAt(27);
+		System.out.println(v);
+		
+		char r = str.charAt(17);
+		System.out.println(r);
+		
+		char a = str.charAt(0);
+		System.out.println(a);
+		
+		char h = str.charAt(7);
+		System.out.println(h);
+		
+		char m = str.charAt(12);
+		System.out.println(m);
+		
+		char aa = str.charAt(0);
+		System.out.println(aa);
+		
+		char hh = str.charAt(7);
+		System.out.println(hh);
+		
+		System.out.print(s + "" + i + "" + t + "" + ii + "" + v + "" + r + "" + a + "" + h + "" + m + "" + aa + "" + hh);
+				
+	}
+}
+
